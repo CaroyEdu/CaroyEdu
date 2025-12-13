7a25fcff9d71996d4e16c5e2befba15ca461ea83
Jean Edouard Roy Caro <roycaroje@gmail.com>
2025-12-13 16:34:02 +0100

    Changed the shape of the presentation

---
+++ src/components/Experience.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+
+function Experience() {
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      sx={{ backgroundColor: "red" }}
+    ></Grid>
+  );
+}
+
+export default Experience;
