67783516343c4d2e7c288aaecb43f20245156277
Jean Edouard Roy Caro <roycaroje@gmail.com>
2025-12-26 21:29:43 +0100

    First addition to i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,6 +4,7 @@ import LanguageDetector from "i18next-browser-languagedetector";
 
 import es from "./es.json";
 import en from "./en.json";
+import fr from "./fr.json";
 
 i18n
   .use(LanguageDetector)
@@ -12,6 +13,7 @@ i18n
     resources: {
       es: { translation: es },
       en: { translation: en },
+      fr: { translation: fr },
     },
     fallbackLng: "es",
     interpolation: {
